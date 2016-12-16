a97d2a397bf5b833d94c8d1f4c767434c79d2a8e
Pavel <kakoka@gmail.com>
2016-12-16 20:11:28 +0300

    bower 1

--- project/settings.py
+++ project/settings.py
@@ -152,7 +152,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     'static/',
-    'bower_components/',
+    'bower_components/'
 ]
 
 CART_PRODUCT_MODEL = 'eshop.models.products.Product'
